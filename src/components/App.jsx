698a1f2bbb22c29787f2399a4972850ed1042994
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-05-18 21:39:36 +0200

    added marker to the map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
-import FlatList from './Flat_List';
-// import SimpleMap from './Google_Map_React';
+import GoogleMapReact from 'google-map-react';
 
+import FlatList from './Flat_List';
 import flats from '../../data/flats';
+import Marker from './Marker';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { flats: flats };
+    this.state = {
+      flats,
+      selectedFlat: flats[0]
+    };
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
   }
 
   render () {
     return (
       <div>
-        <FlatList flats={this.state.flats} />
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+         />
+        <div className="map-container">
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: ENV['GOOGLE_MAPS_KEY'] }}
+            defaultCenter={this.center()}
+            defaultZoom={11} >
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng}/>
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
