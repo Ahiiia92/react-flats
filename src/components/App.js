39cc8c155573c229960e6a380acefde3899137c5
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-05-18 17:18:49 +0200

    components created and displaying hardcoded card

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import FlatList from './Flat_List.js';
+
+import flats from '../../data/flats.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: flats
+    }
+  }
+
   render () {
     return (
       <div>
         Hello, Marou !
+        <div className="right-side">
+        </div>
+        <div className="left-side">
+          <FlatList flats={this.state.flats} />
+        </div>
       </div>
     );
   }
---
+++ src/components/Flat_List.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Flat from './Flat.js';
+
+class FlatList extends Component {
+  // renderList = () => {
+  //   return this.props.flats.map((flat) => {
+  //     <Flat
+  //     name={flat.name}
+  //     key={flat.name}
+  //     imageUrl={flat.imageUrl}
+  //     price={flat.price}
+  //     priceCurrency={flat.priceCurrency}
+  //     lat={flat.lat}
+  //     lng={flat.lng}
+  //     />
+  //   });
+  // }
+
+  render () {
+    return (
+    <div className="flats-list">
+      <Flat />
+    </div>
+      );
+  }
+}
+
+export default FlatList;
